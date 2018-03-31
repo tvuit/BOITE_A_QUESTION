07affcf2b9b52b707dd6054516596a0ff460edaa
Thibaud Vuitton <vuittont@gmail.com>
2018-03-31 16:25:45 +0200

    somme redis setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
+config.action_cable.url = "wss://questionboxes.herokuapp.com/cable"
+config.action_cable.allowed_request_origins = [ "http://questionboxes.herokuapp.com", "https://questionboxes.herokuapp.com" ]
+
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
